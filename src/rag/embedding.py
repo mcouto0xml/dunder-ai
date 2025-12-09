5f46604c7b3dbf9014fde50fbdd48a6d714bff65
mcouto0xml <murilo.oliveira@sou.inteli.edu.br>
2025-12-09 13:32:50 -0300

    feat: refatora e add requirements v2

--- src/rag/embedding.py
+++ src/rag/embedding.py
@@ -1,16 +1,31 @@
 from config import rag, rag_corpus
 
+def resolver_ids_por_nome(nomes_desejados):
+    files = rag.list_files(rag_corpus.name)
 
-def make_embedding():
+    ids = []
+    for f in files:
+        if f.display_name in nomes_desejados:
+            # ✅ Extrai somente o ID curto
+            rag_file_id_curto = f.name.split("/ragFiles/")[-1]
+            ids.append(f"{rag_file_id_curto}")
+
+    if not ids:
+        raise ValueError("Nenhum dos arquivos solicitados foi encontrado no corpus.")
+
+    return ids
+
+
+def make_embedding(text: str, files: list):
     print(rag_corpus.name)
 
-    files = rag.list_files(rag_corpus.name)
+    ids = resolver_ids_por_nome(files)
 
     i = 0
     print("Rodando a iteração sobre arquivos: ")
     for f in files:
         print(i)
-        print("Arquivo:", f.display_name, "| Status:")
+        print("Arquivo:", f)
         i += 1
 
     rag_retrieval_config = rag.RagRetrievalConfig(
@@ -24,11 +39,12 @@ def make_embedding():
                 rag_corpus=rag_corpus.name,
                 # Optional: supply IDs from `rag.list_files()`.
                 # rag_file_ids=["rag-file-1", "rag-file-2", ...],
+                rag_file_ids=ids,
             )
         ],
-        text="Compra acima de US$ 500",
+        text=text,
         rag_retrieval_config=rag_retrieval_config,
     )
     print("Segue a resposta gerada pelo modelo: ", response)
 
-make_embedding()
+make_embedding("Políticas de relacionamento amoroso", ["politica_compliance.txt"])
